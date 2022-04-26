5116ce6a13ce0b2aa99aa01a3518dbcbfe5e222a
gentlemanjs <jungsik321@gmail.com>
2022-04-26 18:45:52 +0900

    2022.04.26

--- src/components/includes/MovieItem.js
+++ src/components/includes/MovieItem.js
@@ -3,8 +3,10 @@ import React from 'react'
 function MovieItem(props) {
     return (
         <li>
-            <img src={`https://image.tmdb.org/t/p/w500/${props.results.poster_path}`} alt="" />
-            <p className="title">{props.results.title}</p>
+            <a href={`https://www.themoviedb.org/movie/${props.results.id}`}>
+                <img src={`https://image.tmdb.org/t/p/w500/${props.results.poster_path}`} alt="" />
+                <p className="title">{props.results.title}</p>
+            </a>
         </li>
     )
 }
